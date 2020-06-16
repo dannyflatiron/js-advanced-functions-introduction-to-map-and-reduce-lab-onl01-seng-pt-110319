1bac77cd0e082bd41796ce1a7c4acede3d601a10
dannyflatiron <dannyflatiron@gmail.com>
2020-06-16 10:07:45 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
 // Your code here
+
+
+const mapToNegativize = function (array) {
+    return array.map(value => value * -1)
+}
+
+const mapToNoChange = function (array) {
+    return array
+}
+
+const mapToDouble = function (array) {
+    return array.map(value => value * 2)
+}
+
+const mapToSquare = function (array) {
+    return array.map(value => value ** 2)
+}
+
+const reducer = (accumulator, item) => {
+    return accumulator + item
+}
+
+const reduceToTotal = function(array, startingPoint=0) {
+    return array.reduce(reducer, startingPoint)
+}
+
+const reduceToAllTrue = function(array) {
+    // if (array.map(value => !value))
+    // return false
+    // else 
+    // return true
+
+    let i 
+    for (i = 0; i < array.length; i++) {
+        if (!array[i])
+        return false
+        
+    }
+    return true
+}
+
+const reduceToAnyTrue = function (array) {
+    let i 
+    for (i = 0; i <array.length; i++) {
+        if (array[i])
+        return true
+    }
+    return false
+}
+
